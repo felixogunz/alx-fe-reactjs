2d5d8f56458b184ac065f6e60b126d3717eb3519
Felix Ogunbowale <167327564+felixogunz@users.noreply.github.com>
2025-02-15 00:48:34 +0100

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -9,6 +9,21 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+
+
+    function WelcomeMessage() {
+    return (
+        <div>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <p>This is a simple JSX component.</p>
+            <p>I am learning about JSX!</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
+    
+    
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
