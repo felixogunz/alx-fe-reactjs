5cc22379deb21283f56f5e0579fbc87ba98f20cd
Felix Ogunbowale <167327564+felixogunz@users.noreply.github.com>
2025-02-15 22:37:07 +0100

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,17 +5,10 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
-import Header from './Header';,
-import MainContent from './MainContent';
-import Footer from './Footer';
-
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-
-    <Header />, <MainContent />, and <Footer />.
-    
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
