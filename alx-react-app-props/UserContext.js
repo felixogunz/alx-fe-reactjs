c646348d2915864e3ebaa3e39aa8ed4fed2b4844
Felix Ogunbowale <167327564+felixogunz@users.noreply.github.com>
2025-02-19 17:46:10 +0100

    Delete alx-react-app-props/UserContext.js

--- alx-react-app-props/UserContext.js
+++
@@ -1,6 +0,0 @@
-import React, { createContext } from 'react';
-
-// Create a context with a default value of an empty object or null.
-const UserContext = createContext();
-
-export default UserContext;
