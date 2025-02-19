e2a9913181392045e52dfbd00c8bbc90fa4e5c39
Felix Ogunbowale <167327564+felixogunz@users.noreply.github.com>
2025-02-19 21:19:03 +0100

    Delete alx-react-app-props directory

--- alx-react-app-props/src/ProfilePage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import UserInfo from './UserInfo';
-
-function ProfilePage() { // Remove userData prop
-  return <UserInfo />;
-}
-
-export default ProfilePage;
--- alx-react-app-props/src/UserInfo.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import UserDetails from './UserDetails';
-
-function UserInfo() { // Remove userData prop
-  return <UserDetails />;
-}
-
-export default UserInfo;
