0e79030c50640a54567693152ae8d1a7f51deb71
Felix Ogunbowale <167327564+felixogunz@users.noreply.github.com>
2025-02-19 23:27:21 +0100

    Create Footer.jsx

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ padding: '10px', backgroundColor: '#333', color: '#fff', textAlign: 'center' }}>
+      <p>&copy; 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
