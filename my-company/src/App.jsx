0f03e5010f669554ce6615425acbaadc845acc36
Felix Ogunbowale <167327564+felixogunz@users.noreply.github.com>
2025-02-19 23:28:02 +0100

    Update App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -24,4 +24,22 @@ function App() {
 
 export default App;
 
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
 
