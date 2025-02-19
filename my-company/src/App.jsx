464c0025547d06b32a3fac5a122b6d8ece684ec8
Felix Ogunbowale <167327564+felixogunz@users.noreply.github.com>
2025-02-19 23:25:21 +0100

    Update App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,2 +1,10 @@
 ["react-router-dom", "Route", "path", "Routes", "element"]
 ["Navbar"]
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';  // We'll create this in the next step
+
