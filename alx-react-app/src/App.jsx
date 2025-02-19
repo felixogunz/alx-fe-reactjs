98e2511b2afd6464561fe2bbf1d463c469a3a738
Felix Ogunbowale <167327564+felixogunz@users.noreply.github.com>
2025-02-19 11:55:43 +0100

    Change

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,33 +1,10 @@
 // src/App.jsx
 import React from 'react';
-import UserProfile from './components/UserProfile';
-
-function App() {
-  return (
-    <div>
-      <h1>User Profile</h1>
-      <UserProfile 
-        name="Alice" 
-        age="25" 
-        bio="Loves hiking and photography" 
-      />
-      <UserProfile 
-        name="Bob" 
-        age="30" 
-        bio="Enjoys cooking and traveling" 
-      />
-    </div>
-  );
-}
-
-export default App;
-
-
-import React from 'react';
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 
+// src/App.jsx
 const App = () => {
   return (
     <div>
