7c531be5106790a48dd99879155fb032e17440b4
Felix Ogunbowale <167327564+felixogunz@users.noreply.github.com>
2025-03-17 17:40:00 +0100

    Initial commit - React with Tailwind setup

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // Can be set to 'media' or 'class' if needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
