fee99f2eb9a7bcc14b91263b211aa4f8d54bdda3
Felix Ogunbowale <167327564+felixogunz@users.noreply.github.com>
2025-03-28 16:14:10 +0100

    Modified githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL = "https://api.github.com/users/";
 const API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
 
-export const fetchGitHubUser = async (username) => {
+export const fetchUserData = async (username) => {
     try {
         const response = await axios.get(`${API_URL}${username}`, {
             headers: API_KEY ? { Authorization: `token ${API_KEY}` } : {},
@@ -11,6 +11,6 @@ export const fetchGitHubUser = async (username) => {
         return response.data;
     } catch (error) {
         console.error("Error fetching GitHub user:", error);
-        return null;
+        throw new Error("User not found");
     }
 };
